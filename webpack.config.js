d6b6e9b48fbad425b7ce0bdf14489c08f09a0b6e
Filip <ioan-leonard.filip@haufe.com>
2021-06-18 19:38:27 +0300

    add css, sass, postcss, hotreloading

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,16 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
 const mode =
   process.env.NODE_ENV === 'production' ? process.env.NODE_ENV : 'development';
 
+// fixes browserslist live reloading bug
+const target = process.env.NODE_ENV === 'production' ? 'browserslist' : 'web';
+
 module.exports = {
   mode,
+  target,
+
+  plugins: [new MiniCssExtractPlugin()],
 
   module: {
     rules: [
@@ -13,11 +21,23 @@ module.exports = {
           loader: 'babel-loader',
         },
       },
+
+      {
+        test: /\.(s[ac]|c)ss$/i,
+        // Webpack reads&processes right-to-left from arrays
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'postcss-loader',
+          'sass-loader',
+        ],
+      },
     ],
   },
 
   devtool: 'source-map',
   devServer: {
     contentBase: './dist',
+    hot: true,
   },
 };
